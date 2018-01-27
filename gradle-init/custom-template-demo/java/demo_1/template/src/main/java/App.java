d3614c0c632ab13d2d06a311fbccf248bb64266a
Maxim Balan <maxim.balan@vonage.com>
2018-01-27 13:39:33 +0000

    adding demo template project

---
+++ gradle-init/custom-template-demo/java/demo_1/template/src/main/java/App.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 27/01/18.
+ *
+ * @author Maxim Balan
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
